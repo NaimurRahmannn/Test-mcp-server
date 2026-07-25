2ff14fa0914a5ab6e8bde2a1c01e8385c24f67ec
unknown <niamurreiman@gmail.com>
2026-07-25 11:38:08 +0600

    feat: expose order total helper

--- src/test_mcp_server/__init__.py
+++ src/test_mcp_server/__init__.py
@@ -1,8 +1,13 @@
 """Small order-service fixture used by OpenSteward integration tests."""
 
 from test_mcp_server.idempotency import IdempotentOrderStore
-from test_mcp_server.orders import Order, create_order
+from test_mcp_server.orders import Order, create_order, order_total
 
-__all__ = ["IdempotentOrderStore", "Order", "create_order"]
+__all__ = [
+    "IdempotentOrderStore",
+    "Order",
+    "create_order",
+    "order_total",
+]
 
 
